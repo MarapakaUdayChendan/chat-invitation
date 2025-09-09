657aece201fe8930eaf352e6bb297bba91ba529d
Ganesh <hareeramaa321@gmail.com>
2025-09-09 17:43:12 +0530

    Added new Screens

---
+++ src/screens/OrganizationSelection.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+const OrganizationSelection : React.FC = ()=> {
+
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+
+}
